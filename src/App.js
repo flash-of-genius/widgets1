2707e9e253ff5bb977a80012db83c517183bae46
flash-of-genius <amhdaar@gmail.com>
2022-07-09 07:16:56 +0100

    38 - Selection State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -17,11 +18,23 @@ const items = [
   },
 ];
 
+const options = [
+  { label: "the Color Red", value: "red" },
+  { label: "The color Green", value: "green" },
+  { label: "a shade of blue", value: "blue" },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
